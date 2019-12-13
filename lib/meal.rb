d8bd996058ac8a8500735774eb06f4f4e43d3cd6
Paul Leugemors <pleugemors@yahoo.com>
2019-12-13 23:41:02 +0000

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,18 @@
 class Meal
-
+  
+  attr_accessor :waiter, :customer, :total, :tip 
+  @@all =[]
+  
+  def initialize(waiter, customer, total, tips)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip =  tips
+    @@all << self 
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
--- spec/01_has_many_through_spec.rb
+++ spec/01_has_many_through_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require 'pry'
 
 describe "Basic class structures" do
   
