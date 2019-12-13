3a66a29d54419afa2f61a56a50b131754cf7e3d3
Paul Leugemors <pleugemors@yahoo.com>
2019-12-13 23:37:38 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -23,13 +23,10 @@ class Waiter
   end
   
   def best_tipper
-    #best_tipped_meal = meals.max do |meal_a, meal_b|
-     # meal_a.tip <=> meal_b.tip
-      
-    best_tipped_meal = meals.max_by{|meal_name| meal_name.tip} 
-   # end
-   #binding.pry
-    best_tipped_meal.customer
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+   best_tipped_meal.customer
   end
   
 end
