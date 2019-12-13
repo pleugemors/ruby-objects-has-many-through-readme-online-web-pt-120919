ef7e20c3b1bd107440afaffd45dee3d364637aba
Paul Leugemors <pleugemors@yahoo.com>
2019-12-13 23:01:13 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -17,4 +17,12 @@ class Customer
     Meal.new(waiter, self, total, tip)
   end
   
+  def meals
+    Meal.all.select{|meal| meal.customer == self}
+  end
+  
+  def waiters
+    meals.map{|meal| meal.waiter}
+  end
+  
 end
